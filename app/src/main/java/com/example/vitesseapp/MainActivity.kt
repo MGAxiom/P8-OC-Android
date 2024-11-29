40672e34936561d8e41702ecc91617591c85d8a5
Maxime Girard <maxime.girard.cpro@gmail.com>
2024-11-29 17:55:30 +0100

    feat(OC-8): added home screen

--- app/src/main/java/com/example/vitesseapp/MainActivity.kt
+++ app/src/main/java/com/example/vitesseapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.vitesseapp
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,37 +12,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.vitesseapp.ui.theme.VitesseAppTheme
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            VitesseAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.homescreen)
+
+        val rootLayout = findViewById<FrameLayout>(R.id.root_layout)
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootLayout) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = insets.top + 15)
+            WindowInsetsCompat.CONSUMED
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VitesseAppTheme {
-        Greeting("Android")
     }
 }
